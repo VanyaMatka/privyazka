f8afaef6e8ecd4e9fd3bb69f491f50fa14bdf9af
VanyaMatka <xzotow@yandex.ru>
2024-10-20 10:47:23 +0500

    Ванька создал проект C# с решением

---
+++ papka_for_zadaniya/zadaniye1/zadaniye1/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace zadaniye1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            string name = nameTextBox.Text;
+
+            if (string.IsNullOrEmpty(name))
+            {
+                MessageBox.Show("Ваше имя не введено!", "Кринж", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                MessageBox.Show($"Ваше имя: {name}", "Поздравляю!", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*"
+            };
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, textBox.Text);
+            }
+        }
+
+        private void FontSizeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            textBox.FontSize = e.NewValue;
+        }
+    }
+}
